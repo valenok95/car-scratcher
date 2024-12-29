8630ce780bfb3d2450ca5d92c613d38b9dfadf2e
valentin.zhuravel <valentin.zhuravel@x5.ru>
2024-12-29 21:44:44 +0300

    добавил проверку курса расчёта через тест контроллер

--- src/main/java/ru/wallentos/carscratcher/controller/TestController.java
+++ src/main/java/ru/wallentos/carscratcher/controller/TestController.java
@@ -3,6 +3,7 @@ package ru.wallentos.carscratcher.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,14 +17,24 @@ import ru.wallentos.carscratcher.service.MoneyRateRestService;
 @RequestMapping("test/api/ui")
 public class TestController {
     private CalculatorService calculatorService;
+    private MoneyRateRestService moneyRateRestService;
     @Autowired
-    public TestController(CalculatorService calculatorService) {
+    public TestController(CalculatorService calculatorService, MoneyRateRestService moneyRateRestService) {
         this.calculatorService = calculatorService;
+        this.moneyRateRestService = moneyRateRestService;
     }
 
     @CrossOrigin
-    @PostMapping
+    @PostMapping("test-calculator")
     public ResponseEntity<CalculatorResponseDto> calculateCarByFullRequest(@RequestBody CalculatorRequestDto calculatorRequestDto) {
         return ResponseEntity.ok(calculatorService.calculateKoreaCarPrice(calculatorRequestDto));
+    }
+    @CrossOrigin
+    @GetMapping("calculator-rates")
+    public ResponseEntity<String> getMoneyConversionRates() {
+        return ResponseEntity.ok(String.format("""
+                Курс расчёта:
+                %s
+                """,moneyRateRestService.getCalculationRatesInRublesMap()));
     }
 }
